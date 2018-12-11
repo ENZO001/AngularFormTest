9026f57e77c8d8b5b8ed946ec9f50e29abd6031d
劉士豪(Enzo Liu) <Z00040150@ctcb.cfhc.com>
2018-12-11 16:13:56 +0800

    model driven form 改成使用 array 可以動態新增刪除

--- src/app/modelDrivenForm/member/member.component.ts
+++ src/app/modelDrivenForm/member/member.component.ts
@@ -12,7 +12,13 @@ export class MemberComponent implements OnInit {
   emailPattern = '^[a-zA-Z0-9.!#$%&*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$';
 
   constructor(private _fb: FormBuilder) {
-    this.form = this._fb.group({
+    this.form = this._fb.array([
+      this.buildGroup()
+    ]);
+  }
+
+  buildGroup(): FormGroup {
+    return this._fb.group({
       // 第一個值是value，第二個是validator，此處的"Validators"是現成的
       chineseName: ['', [Validators.required, Validators.minLength(5)]],
       englishName: ['Enzo', [Validators.required]],
@@ -27,10 +33,32 @@ export class MemberComponent implements OnInit {
         games: ''
       }),
       sex: ''
-    });
+    }, { updateOn: 'blur' }); // 在xx(失焦)的時候，才會觸發change
   }
 
   ngOnInit() {
+  }
+
+  add() {
+    this.form.push(this.buildGroup());
+  }
+
+  remove(idx: number) {
+    this.form.removeAt(idx);
+  }
+
+  insert(idx) {
+    this.form.insert(idx + 1, this.buildGroup());
+  }
+
+  resetFormData() {
+    this.form.reset();
+  }
+
+  clear() {
+    while (this.form.length > 1) {
+      this.form.removeAt(1);
+    }
   }
 
   signUp(form: FormGroup) {
